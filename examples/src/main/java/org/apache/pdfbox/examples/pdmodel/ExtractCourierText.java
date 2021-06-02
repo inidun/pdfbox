c2a2407338aaa8271fec7d4b1b2909799e27f1b0
aibakeneko <aibakeneko@gmail.com>
2021-06-02 16:48:41 +0200

    Changed name

--- examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractCourierText.java
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractCourierText.java
@@ -4,12 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import org.apache.pdfbox.tools.PDFCourier2Text;
 
-public class DisturbingThisIs {
+public class ExtractCourierText {
 
 	public static void main(String[] args) throws Exception {
 
 		PDFCourier2Text stripper = new PDFCourier2Text(5.5f, 8);
-
 		List<String> pages = stripper.extractText("012656engo.pdf");
 		List<List<PDFCourier2Text.TitleInfo>> titles = stripper.getTitles();
 		System.out.println(Arrays.toString(pages.toArray()));
